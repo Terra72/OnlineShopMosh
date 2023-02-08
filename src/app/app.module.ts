0bb53b0286a8dc375b99fc0cc264144696949daa
Daniel Bowden <dbowden72@gmail.com>
2023-02-08 16:01:53 +0100

    Component creation and first look at the front page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
 
-@NgModule({
+
+
+@NgModule({ //this is where we declare our modules , it should auto add it to the imports as well
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/courses.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({ //decorator function 
+    selector: 'courses',
+    template: '<h2>Courses</h2>'
+})
+export class CoursesComponent{
+
+}

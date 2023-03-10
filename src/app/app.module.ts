0277b362fc710426455c3d3c67d7a6d91e5c1704
Daniel Bowden <dbowden72@gmail.com>
2023-03-10 13:30:16 +0100

    Tutorial practice, fiddling with created package files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
+import { CourseComponent } from './course/course.component';
 
 
 
 @NgModule({ //this is where we declare our modules , it should auto add it to the imports as well
   declarations: [
     AppComponent,
-    CoursesComponent
+    CoursesComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,8 +2,22 @@ import {Component} from '@angular/core';
 
 @Component({ //decorator function 
     selector: 'courses',
-    template: '<h2>Courses</h2>'
+    template: `
+    <h2>{{"title: " + getTitle()}}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{course}}
+            </li>
+        </ul>
+    `
 })
 export class CoursesComponent{
+   title = "list of courses"
 
+   getTitle(){
+    return this.title
+   }
+
+//    courses = ["maths", "scintz", "history"] //replaced by service class see courses.services.ts
+    courses:String[] = []
 }
